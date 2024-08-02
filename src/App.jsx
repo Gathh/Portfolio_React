a5a3ceeab93aa6924dd5ad0d6c2772c7b2a042ac
Gathh <rafagmk@gmail.com>
2024-08-01 22:33:59 -0300

    Barra Superior

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,39 @@ function App() {
 
   return (
     <>
-    
+      <nav style={{ position: 'fixed', top: 0, paddingLeft: '2rem', paddingRight: '2rem' }}>
+        <ul>
+          <li>
+            <a href="#home">Home</a>
+          </li>
+          <li>
+            <a href="#tecnico">Técnico</a>
+            <ul>
+              <li>
+                <a href="#desenvolvimento-de-sistemas">Desenvolvimento de Sistemas</a>
+              </li>
+              <li>
+                <a href="#modelagem-de-sistemas">Modelagem de Sistemas</a>
+              </li>
+              <li>
+                <a href="#outras-materias">Outras Matérias</a>
+              </li>
+            </ul>
+          </li>
+          <li>
+            <a href="#materias-escolares">Matérias Escolares</a>
+          </li>
+          <li>
+            <a href="#habilidades">Habilidades</a>
+          </li>
+          <li>
+            <a href="#contato">Contato</a>
+          </li>
+        </ul>
+      </nav>
+      <div>
+        <h1>Bem-vindo ao meu Portfólio!</h1>
+      </div>
     </>
   )
 }
